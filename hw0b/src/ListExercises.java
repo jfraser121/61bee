9e533350c6fd70f5843888a786a6965def673f28
Johnny Fraser <johnnyfraser121@gmail.com>
2023-06-03 11:05:45 +0100

    Complete hw0B ListExercises

--- hw0b/src/ListExercises.java
+++ hw0b/src/ListExercises.java
@@ -1,25 +1,51 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListExercises {
 
     /** Returns the total sum in a list of integers */
-	public static int sum(List<Integer> L) {
-        return 0;
+    public static int sum(List<Integer> L) {
+        int total = 0;
+        for (int number : L) {
+            total += number;
+        }
+        return total;
     }
 
     /** Returns a list containing the even numbers of the given list */
     public static List<Integer> evens(List<Integer> L) {
-        return null;
+        List<Integer> lst = new ArrayList<>();
+        for (int number : L){
+            if (number % 2 == 0) {
+                lst.add(number);
+            }
+        }
+        return lst;
     }
 
     /** Returns a list containing the common item of the two given lists */
     public static List<Integer> common(List<Integer> L1, List<Integer> L2) {
-        return null;
+        List<Integer> lst = new ArrayList<>();
+        for (int number : L1) {
+            if (L2.contains(number)) {
+                lst.add(number);
+            }
+        }
+        return lst;
     }
 
 
     /** Returns the number of occurrences of the given character in a list of strings. */
     public static int countOccurrencesOfC(List<String> words, char c) {
-        return 0;
+        int count = 0;
+        for (String word : words) {
+            for (int i = 0; i < word.length(); i++) {
+                char chr = word.charAt(i);
+                if (c == chr) {
+                    count ++;
+                }
+            }
+        }
+        return count;
     }
 }
